16063bf165b45d41d47b06c6913292b76d27c2ba
hrndbrs <hrndbrs@gmail.com>
2023-09-01 00:35:27 +0700

    0.0.2

--- script/formJualan.js
+++ script/formJualan.js
@@ -5,12 +5,24 @@ const form = document.getElementById("jual-form-container")
 const createSelectInput = (objects, required = true) => {
     const select = document.createElement("select")
     const defaultOption = document.createElement("option")
-
-    select.setAttribute("required", "true")
-
+    
+    select.appendChild(defaultOption)
+    select.setAttribute("id", "state")
+    select.classList.add("form-select")
+    
     if(required) select.setAttribute("required", "true")
-
+    
     for(let obj in objects) {
-
+        const option = document.createElement("option")
+        option.setAttribute("value", obj)
+        option.innerText = objects[obj]
+        select.appendChild(option)
     }
+
+    return select
 }
+
+// const createRadio = (objects) => {
+//     const 
+// }
+
